8b1a865133192a5982ee150b55a1b1b6f6f7f9a8
60 - Tommy ZAFITSIARENDRIKA <zafi.tommy@gmail.com>
2024-04-13 11:09:21 +0300

    TP Banque - Message erreur et succes

--- src/main/java/mg/zafitsiarendrika/tpbanquezafitsiarendrika/jsf/Transfert.java
+++ src/main/java/mg/zafitsiarendrika/tpbanquezafitsiarendrika/jsf/Transfert.java
@@ -8,6 +8,7 @@ import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import mg.zafitsiarendrika.tpbanquezafitsiarendrika.entity.CompteBancaire;
+import mg.zafitsiarendrika.tpbanquezafitsiarendrika.jsf.util.Util;
 import mg.zafitsiarendrika.tpbanquezafitsiarendrika.service.GestionnaireCompte;
 
 /**
@@ -52,10 +53,36 @@ public class Transfert {
     
     
     public String transferer(){
+        boolean erreur = false;
         CompteBancaire source = gestionnaireCompte.findById(idSource);
+        // Si la source n'existe pas
+        if(source == null){
+            erreur = true;
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+        } 
+        // si le solde de la source est insuffisant
+        else{
+            if(source.getSolde() < montant){
+                erreur = true;
+                Util.messageErreur("Solde insuffisant pour le transfert", "Solde insuffisant pour le transfert", "form:source");
+            }
+        }
+        
         CompteBancaire destination = gestionnaireCompte.findById(idDestination);
+        // Si la destination n'existe pas
+        if(destination == null){
+            erreur = true;
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:destination");
+        }
+        
+        // s'il y a des erreurs
+        if(erreur){
+            return null;
+        }
+       
         gestionnaireCompte.transferer(source, destination, montant);
-        return "listeComptes";
+        Util.addFlashInfoMessage("Transfert de "+ montant +" Ar correctement effectue de "+source.getNom()+" vers "+destination.getNom());
+        return "listeComptes?faces-redirect=true";
     }
     /**
      * Creates a new instance of Transfert
