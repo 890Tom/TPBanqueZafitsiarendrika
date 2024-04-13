489f16f476804ca3dde1bd04832b50c776825825
60 - Tommy ZAFITSIARENDRIKA <zafi.tommy@gmail.com>
2024-04-13 09:59:45 +0300

    TP Banque - Template/Menu/Liste des comptes

---
+++ src/main/java/mg/zafitsiarendrika/tpbanquezafitsiarendrika/jsf/ListeComptes.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.zafitsiarendrika.tpbanquezafitsiarendrika.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.zafitsiarendrika.tpbanquezafitsiarendrika.entity.CompteBancaire;
+import mg.zafitsiarendrika.tpbanquezafitsiarendrika.service.GestionnaireCompte;
+
+/**
+ *
+ * @author kk
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+    
+    private List<CompteBancaire> allComptes;
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+    
+    
+    public List<CompteBancaire> getAllComptes(){
+        if(allComptes == null){
+            allComptes = gestionnaireCompte.getAllComptes();
+        }
+        return allComptes;
+    }
+    
+}
