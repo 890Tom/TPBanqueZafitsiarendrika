78f4437f92e307e3852264832ae4ebd410a8a6a4
60 - Tommy ZAFITSIARENDRIKA <zafi.tommy@gmail.com>
2024-04-28 09:22:16 +0300

    TP 5 - n + 1 select

--- src/main/java/mg/zafitsiarendrika/tpbanquezafitsiarendrika/entity/CompteBancaire.java
+++ src/main/java/mg/zafitsiarendrika/tpbanquezafitsiarendrika/entity/CompteBancaire.java
@@ -23,7 +23,7 @@ import java.util.List;
  */
 @Entity
 @NamedQueries({
-    @NamedQuery(name="CompteBancaire.findAll", query = "SELECT c FROM CompteBancaire c "),
+    @NamedQuery(name="CompteBancaire.findAll", query = "SELECT DISTINCT c FROM CompteBancaire c JOIN FETCH c.operations"),
 })
 public class CompteBancaire implements Serializable {
 
